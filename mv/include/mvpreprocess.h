fcabd881541d0e68f61e3c07548f4882b4ec77f7
Darryl Wallace <wallacdj@gmail.com>
2013-12-03 23:39:48 -0500

    Still working on preprocess.

--- mv/include/mvpreprocess.h
+++ mv/include/mvpreprocess.h
@@ -15,6 +15,10 @@
 
 #include "mvmatrix.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef enum MVPreprocessTransformation_e {
     MV_TRANSFORM_NONE = 0,          /*! None */
     MV_TRANSFORM_LINEAR,            /*! Ax + B      */
@@ -47,32 +51,52 @@ typedef struct MVPreprocessColumn_s {
     MVPreprocessCoeffs t_coeffs;
     MVPreprocessCentering c;
     MVPreprocessScaling s;
-    double custom_scaling;
+    double multiplier;
 
-} MVPreprocessColumn;
+} MVPreprocessColumnInfo;
 
 typedef struct MVPreprocessContext_s {
-    MVMat * matrix;                 /*! Input matrix of size (N x M) */
-
-    /* Internal */
-    MVPreprocessColumn *preprocess_info; /*Array of size (1 x M) */
+    MVPreprocessColumnInfo * preprocess_info; /*! Array of size (1 x M) */
+    MVMat * centering;                    /*! Array of size (1 X M) */
+    MVMat * scaling;                      /*! Array of size (1 x M) */
 
 } MVPreprocessContext;
 
 MVPreprocessContext * mvpreprocess_alloc();
-MVPreprocessContext * mvpreprocess_alloc_mat(MVMat *matrix);
-MVPreprocessContext * mvpreprocess_alloc_mcuv(MVMat *matrix);
+MVPreprocessContext * mvpreprocess_alloc_init_mat(int ncolumns);
+MVPreprocessContext * mvpreprocess_alloc_init_mcuv(MVMat *matrix);
 
-int mvpreprocess_init_mat(MVPreprocessContext *ctx, MVMat *matrix);
+int mvpreprocess_init_mat(MVPreprocessContext *ctx, int ncolumns);
 int mvpreprocess_init_mcuv(MVPreprocessContext *ctx, MVMat *matrix);
 
 int mvpreprocess_free(MVPreprocessContext **ctx);
 
 int mvpreprocess_set_column(MVPreprocessContext *ctx, int column,
-                            MVPreprocessColumn *col_info);
-int mvpreprocess_get_column(MVPreprocessContext *ctx, MVPreprocessColumn *col_info_out,
+                            const MVPreprocessColumnInfo *col_info);
+int mvpreprocess_get_column(MVPreprocessContext *ctx, MVPreprocessColumnInfo *col_info_out,
                             int column);
+
+int mvpreprocess_prep_cs(MVPreprocessContext *ctx, MVMat *matrix);
+
+/*! Performs the preprocessing
+    Preprocessing is done in the following order:
+
+        - Functional Transformations
+        - Subtract Centering
+        - Divide by Scaling
+        - Multiplier
+  */
 int mvpreprocess_do(MVPreprocessContext *ctx, MVMat *preprocessed_out, MVMat *raw_in);
+
+/*! Unpreprocesses a matrix
+
+    Unpreprocessing is done in the following order:
+
+        - Divide by multipler
+        - Multiply by scaling
+        - Add centering
+        - Functional inverse transformation
+  */
 int mvpreprocess_undo(MVPreprocessContext *ctx, MVMat *raw_out, MVMat *preprocessed_in);
 
 #endif // MVPREPROCESS_H
--- mv/src/mvpreprocess.c
+++ mv/src/mvpreprocess.c
@@ -12,21 +12,111 @@
 
 
 #include "mvpreprocess.h"
+#include "mvconstants.h"
+
+#include <string.h>
 
 
 MVPreprocessContext * mvpreprocess_alloc()
 {
     MVPreprocessContext *out = malloc(sizeof(MVPreprocessContext));
-    out->matrix = NULL;
     out->preprocess_info = NULL;
+    out->centering = NULL;
+    out->scaling = NULL;
     return out;
 }
 
-MVPreprocessContext * mvpreprocess_alloc_mat(MVMat *matrix)
+MVPreprocessContext * mvpreprocess_alloc_init_mat(int ncolumns)
 {
     MVPreprocessContext *out = mvpreprocess_alloc();
     if (out)
     {
-        out->matrix = matrix;
+        mvpreprocess_init_mat(out, ncolumns);
     }
+    return out;
 }
+
+MVPreprocessContext * mvpreprocess_alloc_mcuv(MVMat *matrix)
+{
+    MVPreprocessContext *out = mvpreprocess_alloc();
+    if (out)
+    {
+        mvpreprocess_init_mcuv(out, matrix);
+    }
+    return out;
+}
+
+int mvpreprocess_init_mat(MVPreprocessContext *ctx, int ncolumns)
+{
+    if (!ctx)
+    {
+        return ERROR;
+    }
+    int size = ncolumns * sizeof(MVPreprocessColumnInfo);
+    ctx->preprocess_info = (MVPreprocessColumnInfo *) malloc(size);
+    memset(ctx->preprocess_info, 0, size);
+    ctx->centering = mvmat_alloc(1, ncolumns);
+    ctx->scaling = mvmat_alloc(1, ncolumns);
+    return SUCCESS;
+}
+
+int mvpreprocess_init_mcuv(MVPreprocessContext *ctx, MVMat *matrix)
+{
+    if (!ctx)
+    {
+        return ERROR;
+    }
+    mvpreprocess_init_mat(ctx, matrix->ncolumns);
+    mvmat_column_mean(ctx->centering, matrix);
+    mvmat_column_stddev(ctx->scaling, matrix, 1);
+    int i;
+    MVPreprocessColumnInfo s = {MV_TRANSFORM_NONE,
+                            {1.0, 1.0, 1.0},
+                            MV_CENTERING_MEAN,
+                            MV_SCALING_UV,
+                            1.0};
+    for (i = 0; i < matrix->ncolumns; i++)
+    {
+        ctx->preprocess_info[i] = s;
+    }
+
+    return SUCCESS;
+}
+
+int mvpreprocess_free(MVPreprocessContext **ctx)
+{
+    if (ctx)
+    {
+        MVPreprocessContext *c = *ctx;
+        if (c)
+        {
+            mvmat_free(&c->centering);
+            mvmat_free(&c->scaling);
+            free(c->preprocess_info);
+            *ctx = NULL;
+            return SUCCESS;
+        }
+    }
+    return ATTEMPT_TO_FREE_NULL_MATRIX;
+}
+
+int mvpreprocess_set_column(MVPreprocessContext *ctx, int column,
+                            const MVPreprocessColumnInfo *col_info)
+{
+    ctx->preprocess_info[column] = *col_info;
+    return SUCCESS;
+}
+
+int mvpreprocess_get_column(MVPreprocessContext *ctx,
+                            MVPreprocessColumnInfo *col_info_out, int column)
+{
+    *col_info_out = ctx->preprocess_info[column];
+    return SUCCESS;
+}
+
+int mvpreprocess_prep_cs(MVPreprocessContext *ctx, MVMat *matrix)
+{
+    (void) ctx; (void) matrix;
+    return SUCCESS;
+}
+
