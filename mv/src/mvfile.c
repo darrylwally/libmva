36b76331b5b8f72426285e865b7d70f89a324ef1
Darryl Wallace <wallacdj@gmail.com>
2014-06-30 22:09:26 -0400

    Intermediate commit for creating file.

--- mv/src/mvfile.c
+++ mv/src/mvfile.c
@@ -21,24 +21,50 @@ static const uint64_t VERSION = 0x000000001;
   uint64_t - version
   uint64_t - number_of_models
   uint64_t * number of models - size in bytes of each model.
-  MVModel * number of models - the mv model data is serialized.
 
   Models are written sequentially based on their size as tightly packed as
   possible.  Start position of each model could be inferred by taking the size
   of each model past the header.
   */
-int  mvfile_write_header(FILE *file, MVModel *models, int num_models)
+int  mvfile_write_header(FILE *file, const MVModel *models, int num_models)
 {
     int i;
     size_t total = fwrite(&VERSION, sizeof(uint64_t), 1, file);
     uint64_t num = (uint64_t) num_models;
     total += fwrite(&num, sizeof(uint64_t), 1, file);
 
-    MVModel * model = models;
+    const MVModel * model = models;
     for (i = 0; i < num_models; i++)
     {
         num = __mvfile_compute_model_size(model);
-        total += fwrite(&num, sizeof(uint64_t, 1, file));
+        total += fwrite(&num, sizeof(uint64_t), 1, file);
         model++;
     }
+    return 0;
+}
+
+static int __mvfile_serialize_mat(FILE *file, const MVMat *mat)
+{
+    fwrite(&mat->nrows, sizeof(mat->nrows), 1, file);
+    fwrite(&mat->ncolumns, sizeof(mat->ncolumns), 1, file);
+    fwrite(mat->data[0], sizeof(double), sizeof(double)*mat->nrows*mat->ncolumns, file);
+    fwrite(mat->mask[0], sizeof(size_t), sizeof(size_t)*mat->nrows*mat->ncolumns, file);
+    return 0;
+}
+
+static int __mvfile_deserialize_mat(MVMat **mat, FILE *file)
+{
+    MVMat *m = NULL;
+    int nrows, ncolumns;
+    fread(&nrows,sizeof(int),1, file);
+    fread(&ncolumns,sizeof(int),1, file);
+
+    if (nrows > 0 && ncolumns > 0)
+    {
+        m = mvmat_alloc(nrows, ncolumns);
+        fread(m->data[0], sizeof(double), sizeof(double)*nrows*ncolumns, file);
+        fread(m->mask[0], sizeof(size_t), sizeof(size_t)*nrows*ncolumns, file);
+    }
+    *mat = m;
+    return 0;
 }
